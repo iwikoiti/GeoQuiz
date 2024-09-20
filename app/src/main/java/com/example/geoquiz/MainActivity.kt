9092973470d30f5b8499935b9a02116b0f5cb97a
yui <yuineor@gmail.com>
2024-09-21 02:37:28 +0700

    add previous button and control direction

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -11,12 +11,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
+    private lateinit var prevButton: Button
     private lateinit var questionTextView: TextView
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -38,6 +40,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
 
 
@@ -51,6 +54,13 @@ class MainActivity : AppCompatActivity() {
 
         nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
+            checkIndexDirection()
+            updateQuestion()
+        }
+
+        prevButton.setOnClickListener{
+            currentIndex = abs(currentIndex - 1) % questionBank.size
+            checkIndexDirection()
             updateQuestion()
         }
 
@@ -73,6 +83,20 @@ class MainActivity : AppCompatActivity() {
         val t = Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
         t.setGravity(Gravity.BOTTOM,0,500)
         t.show()
+    }
+
+    private fun checkIndexDirection(){
+        if (currentIndex == 0){
+            prevButton.visibility = View.INVISIBLE
+        } else {
+            prevButton.visibility = View.VISIBLE
+        }
+
+        if (currentIndex == questionBank.size - 1){
+            nextButton.visibility = View.INVISIBLE
+        } else{
+            nextButton.visibility = View.VISIBLE
+        }
     }
 
 }
